100c31eafb4eded47b55af647091cb01e3a723c2
PC_immuno <PC_immuno>
2017-04-13 20:04:07 +0200

    add symptoms

--- acm_clinical.R
+++ acm_clinical.R
@@ -85,9 +85,14 @@ diagnosis_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.
 diagnosis_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
 
 autoantibody_data = data[,grep("Autoantibody", names(data), value=TRUE)]
-diagnosis_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-diagnosis_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-diagnosis_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
+autoantibody_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
+autoantibody_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
+autoantibody_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
+
+symptom_data = data[,grep("Symptom", names(data), value=TRUE)]
+symptom_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
+symptom_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
+symptom_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
 
 sub_data_list = list(lung_data, kidney_data, nerveSystem_data, skin_data, muscleAndSkeletal_data, comorbidity_data, vascular_data, gastro_data, heart_data, diagnosis_data, autoantibody_data)
 sub_data_name = list("lung", "kidney", "nerveSystem", "skin", "muscleAndSkeletal", "comorbidity", "vascular", "gastro", "heart", "diagnosis", "autoantibody")
