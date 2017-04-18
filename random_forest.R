6f448c7878a73c1f1d264e93bd8e5f3d6769ce3d
Nurtal <nathan.foulquier.pro@gmail.com>
2017-04-18 18:22:38 +0200

    update for phase 2

--- random_forest.R
+++ random_forest.R
@@ -19,7 +19,7 @@ login = info[["login"]]
 #load data
 if(identical(os, "Windows")){
   #-Windows
-  data_file_name = paste("C:\\Users\\", login, "\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\clinical_data_phase_1.csv", sep="")
+  data_file_name = paste("C:\\Users\\", login, "\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\clinical_data_phase_2.csv", sep="")
 }else{
   #-Linux
   data_file_name = ("/home/foulquier/Bureau/SpellCraft/WorkSpace/SCRIPTS/data/clinical_data_phase_1.csv")
@@ -39,6 +39,45 @@ data = data[ , !(names(data) %in% drops_2)]
 drops_3 = c("X.Clinical.Symptom.PEGLOBAL", "X.Clinical.Demography.AGE", "X.Clinical.Consent.CSPHASE", "X.Clinical.Demography.RACE")
 data = data[ , !(names(data) %in% drops_3)]
 
+# drop auto-antibody (non-CALL variables)
+drops_autoantibody = c("X.Autoantibody.SSA60", "X.Autoantibody.U1RNP", "X.Autoantibody.RF","X.Autoantibody.DNA", "X.Autoantibody.SSB", "X.Autoantibody.CENTB", "X.Autoantibody.ENA", "X.Autoantibody.SCL70", "X.Autoantibody.C4", "X.Autoantibody.CLG", "X.Autoantibody.SSA52", "X.Autoantibody.CCP2", "X.Autoantibody.B2M", "X.Autoantibody.CENTB", "X.Autoantibody.C3C", "X.Autoantibody.SSA", "X.Clinical.Lab.LBC3", "X.Autoantibody.CLM", "X.Autoantibody.PR3", "X.Autoantibody.JO1", "X.Autoantibody.SM", "X.Autoantibody.B2G", "X.Autoantibody.PFLC")
+data = data[ , !(names(data) %in% drops_autoantibody)]
+
+# set NA to negative in autoantibody CALL variables
+data$X.Autoantibody.NENACALL[is.na(data$X.Autoantibody.NENACALL)] <- "negative"
+data$X.Autoantibody.ANAPOSNEGCALL[is.na(data$X.Autoantibody.ANAPOSNEGCALL)] <- "negative"
+data$X.Autoantibody.ANCAPOSNEGCALL[is.na(data$X.Autoantibody.ANCAPOSNEGCALL)] <- "negative"
+data$X.Autoantibody.B2GCALL[is.na(data$X.Autoantibody.B2GCALL)] <- "negative"
+data$X.Autoantibody.U1RNPCALL[is.na(data$X.Autoantibody.U1RNPCALL)] <- "negative"
+data$X.Autoantibody.B2MCALL[is.na(data$X.Autoantibody.B2MCALL)] <- "negative"
+data$X.Autoantibody.C3CCALL[is.na(data$X.Autoantibody.C3CCALL)] <- "negative"
+data$X.Autoantibody.C4CALL[is.na(data$X.Autoantibody.C4CALL)] <- "negative"
+data$X.Autoantibody.CCP2CALL[is.na(data$X.Autoantibody.CCP2CALL)] <- "negative"
+data$X.Autoantibody.CENTBCALL[is.na(data$X.Autoantibody.CENTBCALL)] <- "negative"
+data$X.Autoantibody.CLGCALL[is.na(data$X.Autoantibody.CLGCALL)] <- "negative"
+data$X.Autoantibody.CLMCALL[is.na(data$X.Autoantibody.CLMCALL)] <- "negative"
+data$X.Autoantibody.DNACALL[is.na(data$X.Autoantibody.DNACALL)] <- "negative"
+data$X.Autoantibody.ENACALL[is.na(data$X.Autoantibody.ENACALL)] <- "negative"
+data$X.Autoantibody.JO1CALL[is.na(data$X.Autoantibody.JO1CALL)] <- "negative"
+data$X.Autoantibody.MPOCALL[is.na(data$X.Autoantibody.MPOCALL)] <- "negative"
+data$X.Autoantibody.SCL70CALL[is.na(data$X.Autoantibody.SCL70CALL)] <- "negative"
+data$X.Autoantibody.RFCALL[is.na(data$X.Autoantibody.RFCALL)] <- "negative"
+data$X.Autoantibody.PFLCCALL[is.na(data$X.Autoantibody.PFLCCALL)] <- "negative"
+data$X.Autoantibody.PR3CALL[is.na(data$X.Autoantibody.PR3CALL)] <- "negative"
+data$X.Autoantibody.SMCALL[is.na(data$X.Autoantibody.SMCALL)] <- "negative"
+data$X.Autoantibody.SSA52CALL[is.na(data$X.Autoantibody.SSA52CALL)] <- "negative"
+data$X.Autoantibody.SSACALL[is.na(data$X.Autoantibody.SSACALL)] <- "negative"
+data$X.Autoantibody.SSBCALL[is.na(data$X.Autoantibody.SSBCALL)] <- "negative"
+data$X.Autoantibody.SSA60CALL[is.na(data$X.Autoantibody.SSA60CALL)] <- "negative"
+data$X.Autoantibody.U1RNPCALL[is.na(data$X.Autoantibody.U1RNPCALL)] <- "negative"
+
+
+
+
+
+
+
+
 
 # get only the control (a dataframe full of NA ...)
 control_data = data[data$X.Clinical.Diagnosis.DISEASE == "Control", ] 
@@ -47,66 +86,7 @@ control_data = data[data$X.Clinical.Diagnosis.DISEASE == "Control", ]
 data = data[complete.cases(data),]
 data <- data.frame(lapply(data, as.factor))
 
-# Create sub data
-lung_data = data[,grep("Lung", names(data), value=TRUE)]
-#lung_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#lung_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#lung_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-lung_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
 
-kidney_data = data[,grep("Kidney", names(data), value=TRUE)]
-#kidney_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#kidney_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#kidney_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-kidney_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
-
-nerveSystem_data = data[,grep("NerveSystem", names(data), value=TRUE)]
-#nerveSystem_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#nerveSystem_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#nerveSystem_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-nerveSystem_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
-
-skin_data = data[,grep("Skin", names(data), value=TRUE)]
-#skin_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#skin_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#skin_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-skin_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
-
-muscleAndSkeletal_data = data[,grep("MuscleandSkeletal", names(data), value=TRUE)]
-#muscleAndSkeletal_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#muscleAndSkeletal_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#muscleAndSkeletal_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-muscleAndSkeletal_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
-
-comorbidity_data = data[,grep("Comorbirdity", names(data), value=TRUE)]
-#comorbidity_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#comorbidity_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#comorbidity_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-comorbidity_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
-
-vascular_data = data[,grep("Vascular", names(data), value=TRUE)]
-#vascular_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#vascular_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#vascular_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-vascular_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
-
-gastro_data = data[,grep("Gastro", names(data), value=TRUE)]
-#gastro_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#gastro_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#gastro_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-gastro_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
-
-heart_data = data[,grep("Heart", names(data), value=TRUE)]
-#heart_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#heart_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#heart_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-heart_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
-
-diagnosis_data = data[,grep("Diagnosis", names(data), value=TRUE)]
-#diagnosis_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#diagnosis_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#diagnosis_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-diagnosis_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
 
 autoantibody_data = data[,grep("Autoantibody", names(data), value=TRUE)]
 autoantibody_data = data[,grep("CALL", names(autoantibody_data), value=TRUE)]
@@ -115,14 +95,6 @@ autoantibody_data = data[,grep("CALL", names(autoantibody_data), value=TRUE)]
 #autoantibody_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
 autoantibody_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
 
-symptom_data = data[,grep("Symptom", names(data), value=TRUE)]
-#symptom_data$X.Clinical.Diagnosis.DISEASE = as.factor(data$X.Clinical.Diagnosis.DISEASE)
-#symptom_data$X.Clinical.Demography.SEX = as.factor(data$X.Clinical.Demography.SEX)
-#symptom_data$X.Clinical.Sampling.CENTER = as.factor(data$X.Clinical.Sampling.CENTER)
-symptom_data$X.Clinical.Sampling.OMICID = as.factor(data$X.Clinical.Sampling.OMICID)
-
-sub_data_list = list(lung_data, kidney_data, nerveSystem_data, skin_data, muscleAndSkeletal_data, comorbidity_data, vascular_data, gastro_data, heart_data, diagnosis_data)
-sub_data_name = list("lung", "kidney", "nerveSystem", "skin", "muscleAndSkeletal", "comorbidity", "vascular", "gastro", "heart", "diagnosis")
 
 
 #-------------------#
@@ -187,8 +159,12 @@ labels = data$X.Clinical.Diagnosis.DISEASE
 
 
 # drop a few  variable
-drops_3 = c("X.Clinical.Sampling.OMICID", "X.Clinical.Diagnosis.DISEASE")
+drops_3 = c("X.Clinical.Sampling.OMICID", "X.Clinical.Diagnosis.DISEASE", "X.Clinical.Sampling.CENTER")
+drops_autoantibody = c("X.Autoantibody.SSA60", "X.Autoantibody.U1RNP", "X.Autoantibody.RF","X.Autoantibody.DNA", "X.Autoantibody.SSB", "X.Autoantibody.CENTB", "X.Autoantibody.ENA", "X.Autoantibody.SCL70", "X.Autoantibody.C4", "X.Autoantibody.CLG", "X.Autoantibody.SSA52", "X.Autoantibody.CCP2", "X.Autoantibody.B2M", "X.Autoantibody.CENTB", "X.Autoantibody.C3C", "X.Autoantibody.SSA", "X.Clinical.Lab.LBC3")
 selected_data = selected_data[ , !(names(selected_data) %in% drops_3)]
+selected_data = selected_data[ , !(names(selected_data) %in% drops_autoantibody)]
+
+
 
 # run random forest
 data.rf <- randomForest(selected_data, labels)
