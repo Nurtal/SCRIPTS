69b656baedaa9e01c359a326f79411c64068200b
Nurtal <nathan.foulquier.pro@gmail.com>
2017-05-04 10:52:37 +0200

    filter rules

--- rule_mining.R
+++ rule_mining.R
@@ -22,7 +22,7 @@ data <- read.csv(data_file_name, stringsAsFactors=TRUE)
 
 ## variable to keep
 variable_to_keep <- scan("C:\\Users\\PC_immuno\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\feature_selection_diag_RA_formated.txt", what="", sep="\n")
-variable_to_keep <- scan("C:\\Users\\NaturalKiller01\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\feature_selection_diag_RA_formated.txt", what="", sep="\n")
+variable_to_keep <- scan("C:\\Users\\NaturalKiller01\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\feature_selection_diag_SjS_formated.txt", what="", sep="\n")
 variable_to_keep <- c(variable_to_keep, "X.Clinical.Diagnosis.DISEASE")
 data = data[ , (names(data) %in% variable_to_keep)]
 
@@ -39,8 +39,8 @@ data <- data.frame(lapply(data, as.factor))
 # mining
 rules <- apriori(data)
 rules <- apriori(data,
-                 parameter = list(minlen=2, supp=0.14, conf=0.8),
-                 appearance = list(rhs=c("X.Clinical.Diagnosis.DISEASE=RA"),
+                 parameter = list(minlen=2, supp=0.05, conf=0.8),
+                 appearance = list(rhs=c("X.Clinical.Diagnosis.DISEASE=SjS"),
                                    default="lhs"),
                  control = list(verbose=T))
 rules.sorted <- sort(rules, by="lift")
@@ -49,21 +49,28 @@ inspect(rules)
 
 # write rules into file
 rules_df = as(rules, "data.frame");
-write.table(rules_df, "C:\\Users\\NaturalKiller01\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\rules_RA.txt", sep=";")
+write.table(rules_df, "C:\\Users\\NaturalKiller01\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\rules_SjS.txt", sep=";")
 write.table(rules_df, "C:\\Users\\PC_immuno\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\rules_RA.txt", sep=";")
 
 
 # find redundant rules
-# problem there, select all rules
-subset.matrix <- is.subset(rules.sorted, rules.sorted)
-subset.matrix[lower.tri(subset.matrix, diag=T)] <- NA
-redundant <- colSums(subset.matrix, na.rm=T) >= 1
-which(redundant)
+## for better comparison we sort the rules by confidence and add Bayado's improvement
+rules <- sort(rules, by = "confidence")
+quality(rules)$improvement <- interestMeasure(rules, measure = "improvement")
+inspect(rules)
+is.redundant(rules)
 
 # remove redundant rules
-rules.pruned <- rules.sorted[!redundant]
-inspect(rules.pruned)
+rules_filtered <- rules[!is.redundant(rules)]
+inspect(rules_filtered)
 
 # plot stuff
-plot(rules)
+plot(rules_filtered)
 plot(rules, method="graph", control=list(type="items"))
+plot(rules, method="paracoord", control=list(reorder=TRUE))
+plot(rules, method="graph", control=list(type="itemsets"))
+
+# write rules into file
+rules_df = as(rules, "data.frame");
+write.table(rules_df, "C:\\Users\\NaturalKiller01\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\rules_non_redundant_SjS.txt", sep=";")
+write.table(rules_df, "C:\\Users\\PC_immuno\\Desktop\\Nathan\\SpellCraft\\SCRIPTS\\data\\rules_non_redundant_RA.txt", sep=";")
